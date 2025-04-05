c5f0e32b2efa9bb4813a7faa70b6812f653f3e92
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-04-05 20:31:49 +0900

    Update Main.java

--- ABC_385/a/Main.java
+++ ABC_385/a/Main.java
@@ -3,26 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String [] args) {
         try(Scanner sc = new Scanner(System.in)){
-            int n =sc.nextInt();
-            int x = sc.nextInt();
-            int ans = 0;
-            int[] ar = new int[n];
-            for(int i = 0 ; i < n ; i++){
-                ar[i] = sc.nextInt();
-            }
-            for(int i = 0 ;i < n; i++){
-                if(ar[i] == x){
-                    ans = i;
-                    break;
-                }
-            }
-            System.out.println(ans+1);
+            int a = sc.nextInt(), b = sc.nextInt(),c = sc.nextInt();
 
+            String ans = a == b&& b == c || (a + b) == c || (a + c) == b || (b + c) == a ? "Yes" : "No";
+            System.out.println(ans);
         }
     }
-    public static void swap(Object[] a ,int i ,int j) {
-        Object tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-        }
 }
