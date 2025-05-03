f5b53739aa35ab3248c33212654b834f5eb9b5da
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-03 20:04:31 +0900

    Update A.java

--- ABC305/A.java
+++ ABC305/A.java
@@ -1,1 +1,23 @@
 
+/**
+ * 5の倍数に切り上げる方法
+ */
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int ans = 0;
+
+            ans = (n+2)/5 *5;
+            if(ans >100){
+                ans = 100;
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
