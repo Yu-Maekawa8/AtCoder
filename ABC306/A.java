1f29bc29a6f27c029193c26832909bbbc4e0eb5c
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-04 15:39:23 +0900

    Update A.java
    completed

--- ABC306/A.java
+++ ABC306/A.java
@@ -1,1 +1,25 @@
+/*
+* 与えられた文字列の長さを 長さｎから２ｎにする
+*ex. atcoder => aattccooddeerr
+*/
 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0 ; i < n ; i++){
+                for (int j = 0; j < 2; j++) {
+                    sb.append(s.charAt(i));
+                }
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+}
