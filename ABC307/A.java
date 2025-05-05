b304dfacdc0cda4513dd1d1cb103cb39ce3838c0
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-05 18:01:11 +0900

    Update A.java

--- ABC307/A.java
+++ ABC307/A.java
@@ -1,1 +1,27 @@
+/*
+*1週間ごとの歩数合計を出力する
+*/
 
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int size = sc.nextInt();
+            int total = 0;
+            int[] ar = new int[7*size];
+            for (int i = 0; i < 7*size; i++) {
+                ar[i] = sc.nextInt();
+            }
+
+            for(int day = 0 ; day < 7*size ; day++){
+                total += ar[day];
+                if((day+1) % 7 == 0 ){
+                    System.out.print(total + " ");
+                    total = 0;
+                }
+            }
+        }
+    }
+}
