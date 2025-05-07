a6522cbf2f830fece29e94d9b78f90f3568a5997
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-07 16:43:51 +0900

    Create A.java(completed)

---
+++ ABC145/A.java
@@ -0,0 +1,16 @@
+/*
+*半径１の円と比べ　半径nの円は何倍？？
+*
+*/
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int r = sc.nextInt();
+
+            System.out.println(r * r);
+        }
+    }
+}
