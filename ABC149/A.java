3d4f814ebee47fa8f62fa565e4a96728bc252ed0
Y.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-07 15:09:08 +0900

    completed

---
+++ ABC149/A.java
@@ -0,0 +1,15 @@
+package abc149;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            String str1 = sc.next();
+            String str2 = sc.next();
+
+            System.out.println(str2+str1);
+        }
+    }
+}
