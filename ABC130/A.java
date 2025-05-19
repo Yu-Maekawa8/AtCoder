050575a708eb1489fec6de205e469bad729306fb
Yusei.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-19 21:43:28 +0900

    Update A.java

---
+++ ABC130/A.java
@@ -0,0 +1,21 @@
+/*
+*if文
+*
+*
+*/
+
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int a = sc.nextInt();
+    
+    if(num < a){
+      System.out.println(0);
+    }else{
+      System.out.println(10);
+    }
+  }
+}  
