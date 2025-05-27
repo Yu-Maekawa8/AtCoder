1ca471fcb237ec4ec2d5bc0b54c21daf4399d47e
Yusei.Maekawa <vhsiseeuu0fpfc0tz39e@docomo.ne.jp>
2025-05-27 16:48:56 +0900

    Update and rename B.java to Bno.java

---
+++ ABC314/Bno.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int n = sc.nextInt();
+            List<List<Integer>> list = new ArrayList<>();
+            while(sc.hasNext()){
+                int sn = sc.nextInt();
+                List<Integer> sublist = new ArrayList<>();
+                for (int i = 0; i < sn; i++) {
+                    sublist.add(sc.nextInt());
+                }
+                list.add(sublist);
+            }
+            int tg = sc.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                
+            }
+
+        }
+    }
+}
